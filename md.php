c40697880240f7acd68bc2d846aa4fde561f7c03
無常 <3142324836@qq.com>
2023-07-08 18:53:58 +0800

    Fixed bugs

--- md.php
+++ md.php
@@ -5,8 +5,8 @@ function curl_raw($url, $content, $token) {
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_HTTPHEADER,
         array("Accept: application/vnd.github+json",
-                "Authorization: Bearer " . $token),
-                "X-GitHub-Api-Version: 2022-11-28");
+                "Authorization: Bearer " . $token,
+                "X-GitHub-Api-Version: 2022-11-28"));
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
