98ad3dff8207ca8776373ff4b526804ae8f1f4d0
無常 <3142324836@qq.com>
2023-07-09 10:48:01 +0800

    Update js

--- src/render.js
+++ src/render.js
@@ -1,9 +1,21 @@
 function load(path) {
-    
+    let Http = new XMLHttpRequest();
+    Http.open("GET", path);
+    Http.send();
+
+    Http.onreadystatechange = () => {
+        if(Http.readyState == 4 && Http.status == 200) {
+            document.getElementById('render-markdown').innerHTML = Http.responseText;
+        }
+        else {
+            document.getElementById('render-markdown').innerHTML
+                = "HTTP GET ERROR<br>State: " + Http.readyState + "<br>Status: " + Http.status + "<br>" + Http.responseText;
+        }
+    };
 }
 
 function render(data) {
 
 }
 
-document.getElementById('render-markdown').innerHTML = mdPath;
+load(mdPath);
