377b1538bec5da4e63dcf469614d0c7d23f3d7e5
無常 <3142324836@qq.com>
2023-07-09 10:53:02 +0800

    Update js

--- src/render.js
+++ src/render.js
@@ -1,21 +1,25 @@
-function load(path) {
+function load(path, callback, errorHandler) {
     let Http = new XMLHttpRequest();
     Http.open("GET", path);
     Http.send();
 
     Http.onreadystatechange = () => {
         if(Http.readyState == 4 && Http.status == 200) {
-            document.getElementById('render-markdown').innerHTML = Http.responseText;
+            callback(Http.responseText);
         }
         else {
-            document.getElementById('render-markdown').innerHTML
-                = "HTTP GET ERROR<br>State: " + Http.readyState + "<br>Status: " + Http.status + "<br>" + Http.responseText;
+            errorHandler("HTTP GET ERROR<br>State: " + Http.readyState + "<br>Status: " + Http.status + "<br>" + Http.responseText);
+            
         }
     };
 }
 
 function render(data) {
-
+    document.getElementById('render-markdown').innerHTML = data;
 }
 
-load(mdPath);
+function showError(mes) {
+    document.getElementById('render-markdown').innerHTML = mes;
+}
+
+load(mdPath, render, showError);
