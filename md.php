4c703f59ca16b1319771798be09c013696e74a3c
無常 <3142324836@qq.com>
2023-07-09 11:55:46 +0800

    Remove unused codes in php

--- md.php
+++ md.php
@@ -1,49 +1,6 @@
 <?php
-function curl_raw($url, $content, $token) {
-    $accept_header = "Accept: application/vnd.github+json";
-    $token_header = "Authorization: Bearer " . $token;
-    $version_header = "X-GitHub-Api-Version: 2022-11-28";
-
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array($accept_header, $version_header, $token_header));
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-    $json_response = curl_exec($curl);
-
-    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-    curl_close($curl);
-
-    switch ($status) {
-        case 200:  # OK
-          break;
-        default:
-          return "Unexpected HTTP code: " . strval($status) . "\n" . $json_response;
-      }
-
-    return $json_response;
-}
-
-#$token_text = file_get_contents('tokens/api-token.txt');
-#$token_text = str_replace(PHP_EOL, '', $token_text);
-
 $markdown_filename = $_GET['f'];
 $load_url = "/load.php?f=" . $markdown_filename;
-
-#$markdown_text = file_get_contents($markdown_filename);
-
-#$render_url = 'https://api.github.com/markdown';
-
-#$request_array['text'] = $markdown_text;
-#$request_array['mode'] = 'markdown';
-
-#$html_article_body = curl_raw($render_url, json_encode($request_array), $token_text);
-#$html_article_body = $markdown_text;
 
 $header_text = file_get_contents('src/header.html');
 $footer_text = file_get_contents('src/footer.html');
