0453dcff8799241b91f81ab5bca53ad0179a8fca
無常 <3142324836@qq.com>
2023-07-14 16:59:21 +0800

    Update pull.php

--- pull.php
+++ pull.php
@@ -4,6 +4,7 @@ $pull_key = $_GET['key'];
 $key_content = file_get_contents('tokens/pull-key.txt');
 $key_content = str_replace(PHP_EOL, '', $key_content);
 
+# Add 'www-data ALL=(root) NOPASSWD:/usr/bin/git' to /etc/sudoers first.
 if ($pull_key === $key_content) {
     echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="utf-8">';
     echo '<title>' . 'Warsic 音乐社团 - 已更新网站' . '</title>';
