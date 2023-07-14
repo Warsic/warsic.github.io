de6f3b0392e7af9d4705c7d7c06d800f8f8757b5
無常 <3142324836@qq.com>
2023-07-14 16:51:53 +0800

    Update pull.php

--- pull.php
+++ pull.php
@@ -5,7 +5,12 @@ $key_content = file_get_contents('tokens/pull-key.txt');
 $key_content = str_replace(PHP_EOL, '', $key_content);
 
 if ($pull_key === $key_content) {
+    echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="utf-8">';
+    echo '<title>' . 'Warsic 音乐社团 - 已更新网站' . '</title>';
+    echo '</head>';
+    echo '<body>';
     echo exec("sudo git pull");
+    echo '</body></html>';
 }
 else {
     http_response_code(401);
