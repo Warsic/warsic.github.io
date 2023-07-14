130dcf613ba70b4bf4d3afae0d025b395749cb2b
無常 <3142324836@qq.com>
2023-07-14 16:48:26 +0800

    Update pull.php

--- pull.php
+++ pull.php
@@ -5,7 +5,7 @@ $key_content = file_get_contents('tokens/pull-key.txt');
 $key_content = str_replace(PHP_EOL, '', $key_content);
 
 if ($pull_key === $key_content) {
-    passthru("sudo git pull");
+    echo exec("sudo git pull");
 }
 else {
     http_response_code(401);
